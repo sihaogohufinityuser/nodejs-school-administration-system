f11be2fefcf587335fd49e1016b53fa53903be7e
Goh Si Hao <sihao.goh@ufinity.com>
2021-06-18 11:21:36 +0800

    Add names for foreign keys

--- typescript/database/05-TABLE-TeacherStudentClassSubjectMapping.sql
+++ typescript/database/05-TABLE-TeacherStudentClassSubjectMapping.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS TeacherStudentClassSubjectMapping;
-
 CREATE TABLE IF NOT EXISTS TeacherStudentClassSubjectMapping (
     teacherId INT(10) NOT NULL,
     studentId INT(10) NOT NULL,
@@ -8,13 +7,9 @@ CREATE TABLE IF NOT EXISTS TeacherStudentClassSubjectMapping (
     active BOOLEAN NOT NULL DEFAULT TRUE,
     createdAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updatedAt TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY(teacherId,studentId,classId,subjectId),
-    FOREIGN KEY(teacherId)
-        REFERENCES Teacher(id),
-    FOREIGN KEY(studentId)
-        REFERENCES Student(id),
-    FOREIGN KEY(classId)
-        REFERENCES Class(id),
-    FOREIGN KEY(subjectId)
-        REFERENCES Subject(id)
+    PRIMARY KEY(teacherId, studentId, classId, subjectId),
+    CONSTRAINT fk_TeacherStudentClassSubjectMapping_Teacher_teacherId_id FOREIGN KEY(teacherId) REFERENCES Teacher(id),
+    CONSTRAINT fk_TeacherStudentClassSubjectMapping_Student_studentId_id FOREIGN KEY(studentId) REFERENCES Student(id),
+    CONSTRAINT fk_TeacherStudentClassSubjectMapping_Class_classId_id FOREIGN KEY(classId) REFERENCES Class(id),
+    CONSTRAINT fk_TeacherStudentClassSubjectMapping_Subject_subjectId_id FOREIGN KEY(subjectId) REFERENCES Subject(id)
 );
