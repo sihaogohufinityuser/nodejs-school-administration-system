59351f5e18b40c591ffdaef660832449d2a06be0
Goh Si Hao <sihao.goh@ufinity.com>
2021-06-21 00:24:16 +0800

    Unit Test (partial) for UpdateClassNameService

---
+++ typescript/src/services/__tests__/UpdateClassNameService.test.ts
@@ -0,0 +1,34 @@
+import {
+  validateUpdateClassNameRequest,
+  updateClassNameByClassCode,
+} from 'services/UpdateClassNameService';
+
+describe('testing UpdateClassNameService', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  test('validateUpdateClassNameRequest should return BAD_REQUEST error when either parameters is empty', () => {
+    expect(() => {
+      validateUpdateClassNameRequest('', 'P1-1');
+    }).toThrowError(new Error('BAD_REQUEST'));
+    expect(() => {
+      validateUpdateClassNameRequest('P1-1', '');
+    }).toThrowError(new Error('BAD_REQUEST'));
+    expect(() => {
+      validateUpdateClassNameRequest('', '');
+    }).toThrowError(new Error('BAD_REQUEST'));
+    expect(() => {
+      validateUpdateClassNameRequest('P1-1', 'P1 Integrity');
+    }).not.toThrow();
+  });
+
+  /* test('updateClassNameByClassCode should return BAD_REQUEST error when class cannot be found', () => {
+    expect(() => {
+      updateClassNameByClassCode(
+        'class code that cannot be found',
+        'P1 Integrity'
+      );
+    }).toThrowError(new Error('BAD_REQUEST'));
+  }); */
+});
