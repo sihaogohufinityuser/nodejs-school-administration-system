b2b3be616f4f0b07aa234bab172aba7f64a1f369
Goh Si Hao <sihao.goh@ufinity.com>
2021-07-08 01:57:37 +0800

    Add MyClass page with feature to retrieve students list by classCode

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -8,6 +8,7 @@ import ClassList from './components/ClassList';
 import StudentList from './components/StudentList';
 import SubjectList from './components/SubjectList';
 import TeachingRecords from './components/TeachingRecords';
+import MyClass from './components/MyClass';
 
 const App: React.FC = (): JSX.Element => {
   return (
@@ -45,6 +46,11 @@ const App: React.FC = (): JSX.Element => {
                 <Nav.Link>Teaching Records</Nav.Link>
               </IndexLinkContainer>
             </Nav.Item>
+            <Nav.Item>
+              <IndexLinkContainer to={'/my-class'}>
+                <Nav.Link>My Class</Nav.Link>
+              </IndexLinkContainer>
+            </Nav.Item>
           </Nav>
           <hr />
           <Switch>
@@ -54,6 +60,7 @@ const App: React.FC = (): JSX.Element => {
             <Route path="/class-list" component={ClassList} />
             <Route path="/subject-list" component={SubjectList} />
             <Route path="/teaching-records" component={TeachingRecords} />
+            <Route path="/my-class/:classCode?/:pageId?" component={MyClass} />
           </Switch>
         </header>
       </div>
---
+++ react-typescript/src/components/MyClass.tsx
@@ -0,0 +1,205 @@
+import * as React from 'react';
+import { RouteComponentProps, withRouter } from 'react-router';
+import { Alert, Form, Table, Pagination, Button } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import axios from 'axios';
+
+const MAX_STUDENTS_PER_PAGE = 10;
+
+type MyClassParams = {
+  classCode: string;
+  pageId: string;
+};
+
+type MyClassProps = RouteComponentProps<MyClassParams>;
+
+class MyClass extends React.Component<
+  MyClassProps,
+  {
+    classCodeFormInput: string;
+    studentsCount: number;
+    studentsList: string;
+    pageItems: [];
+    is200: boolean | null;
+    is400: boolean | null;
+    is500: boolean | null;
+  }
+> {
+  constructor(props: MyClassProps) {
+    super(props);
+
+    this.onChangeClassCode = this.onChangeClassCode.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+
+    this.state = {
+      classCodeFormInput: '',
+      studentsCount: 0,
+      studentsList: '',
+      pageItems: [],
+      is200: null,
+      is400: null,
+      is500: null,
+    };
+  }
+
+  componentDidUpdate(prevProps: MyClassProps) {
+    if (
+      this.props.match.params.classCode !== prevProps.match.params.classCode ||
+      this.props.match.params.pageId !== prevProps.match.params.pageId
+    ) {
+      if (!this.props.match.params.pageId) {
+        this.props.history.push(
+          `/my-class/${this.props.match.params.classCode}/1`
+        );
+      } else {
+        this.retrieveStudentsList(
+          this.props.match.params.classCode,
+          parseInt(this.props.match.params.pageId)
+        );
+      }
+    }
+  }
+
+  onChangeClassCode(e: { target: { value: any } }) {
+    this.setState({
+      classCodeFormInput: e.target.value,
+      is200: null,
+      is400: null,
+      is500: null,
+    });
+  }
+
+  onSubmit(e: { preventDefault: () => void }) {
+    e.preventDefault();
+
+    this.props.history.push(`/my-class/${this.state.classCodeFormInput}/1`);
+  }
+
+  generateTableList(students: [], pageId: number): any {
+    let studentsArray: any[] = [];
+    for (let index = 0; index < students.length; index++) {
+      const student: any = students[index];
+      studentsArray.push(
+        React.createElement(
+          'tr',
+          {},
+          React.createElement(
+            'td',
+            {},
+            index + 1 + (pageId - 1) * MAX_STUDENTS_PER_PAGE
+          ),
+          React.createElement('td', {}, student.id),
+          React.createElement('td', {}, student.name),
+          React.createElement('td', {}, student.email),
+          React.createElement('td', {}, student.group)
+        )
+      );
+    }
+    return React.createElement('tbody', {}, studentsArray);
+  }
+
+  generatePageItems(count: number, pageId: number): any {
+    const numberOfPages = Math.ceil(count / MAX_STUDENTS_PER_PAGE);
+    const pageItemsArray: any[] = [];
+    for (let index = 0; index < numberOfPages; index++) {
+      const toLink = `/my-class/${this.props.match.params.classCode}/${
+        index + 1
+      }`;
+      pageItemsArray.push(
+        <LinkContainer to={toLink}>
+          <Pagination.Item key={index + 1} active={index + 1 === pageId}>
+            {index + 1}
+          </Pagination.Item>
+        </LinkContainer>
+      );
+    }
+    return pageItemsArray;
+  }
+
+  retrieveStudentsList(classCode: string, pageId: number): void {
+    const offset = (pageId - 1) * MAX_STUDENTS_PER_PAGE;
+    axios
+      .get(
+        `http://localhost:3000/api/class/${classCode}/students?offset=${offset}&limit=${MAX_STUDENTS_PER_PAGE}`
+      )
+      .then((res) => {
+        //console.log(res.data);
+        this.setState({
+          is200: true,
+          studentsCount: res.data.count,
+          studentsList: this.generateTableList(
+            res.data.students,
+            parseInt(this.props.match.params.pageId)
+          ),
+          pageItems: this.generatePageItems(
+            res.data.count,
+            parseInt(this.props.match.params.pageId)
+          ),
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+        if (error.response.status === 400) {
+          this.setState({ is400: true });
+        }
+        if (error.response.status === 500) {
+          this.setState({ is500: true });
+        }
+      });
+  }
+
+  render() {
+    return (
+      <div className="my-class">
+        {this.state.is200 ? (
+          <Alert variant="success">
+            You have successfully retrieved the Students List.
+          </Alert>
+        ) : this.state.is400 ? (
+          <Alert variant="danger">
+            Class cannot be found, or your request contain empty values.
+          </Alert>
+        ) : this.state.is500 ? (
+          <Alert variant="danger">
+            An error has occurred, please check your request and try again.
+          </Alert>
+        ) : (
+          ''
+        )}
+        <h1>Retrieve My Class</h1>
+        <Form onSubmit={this.onSubmit}>
+          <Form.Group controlId="formClassCode">
+            <Form.Label>Class Code</Form.Label>
+            <Form.Control
+              required
+              type="text"
+              value={this.state.classCodeFormInput}
+              onChange={this.onChangeClassCode}
+              placeholder="Enter class code"
+            />
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form.Group>
+        </Form>
+        <h1>Students List</h1>
+        <h5>Students Count: {this.state.studentsCount}</h5>
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Group</th>
+            </tr>
+          </thead>
+          {this.state.studentsList}
+        </Table>
+        <Pagination>{this.state.pageItems}</Pagination>
+      </div>
+    );
+  }
+}
+
+export default withRouter(MyClass);
